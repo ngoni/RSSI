e3a90e3e39894bfed67e1897ae9ca709fb987991
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2022-04-02 11:25:53 +0200

    Request required permissions

--- app/src/main/java/com/scribblex/rssi/ui/MainActivity.kt
+++ app/src/main/java/com/scribblex/rssi/ui/MainActivity.kt
@@ -1,28 +1,45 @@
 package com.scribblex.rssi.ui
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.scribblex.rssi.R
+
 import com.scribblex.rssi.databinding.ActivityMainBinding
 import com.scribblex.rssi.services.RSSIBackgroundService
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import dagger.hilt.android.AndroidEntryPoint
+
 
 private const val TAG = "MainActivity"
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var backgroundServiceIntent: Intent
 
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val PERMISSION_LOCATION_WIFI_STATE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        startBackgroundService()
+        // request permissions
+        // assumptions: we are only handling the success use-case for requesting permissions
+        if (hasRequiredPermissions()) {
+            startBackgroundService()
+        } else {
+            requestRequiredPermissions()
+        }
     }
 
     override fun onDestroy() {
@@ -40,6 +57,51 @@ class MainActivity : AppCompatActivity() {
     private fun cancelBackgroundService() {
         Log.d(TAG, "Stopping background service")
         stopService(backgroundServiceIntent)
+    }
+
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        Log.d(TAG, "Required permissions denied!!")
+        Toast.makeText(this, "Required permissions denied!!", Toast.LENGTH_SHORT).show()
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(this).build().show()
+        } else {
+            startBackgroundService()
+        }
+
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        Log.d(TAG, "Required permissions granted!!")
+        startBackgroundService()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    private fun hasRequiredPermissions() = EasyPermissions.hasPermissions(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.CHANGE_WIFI_STATE
+    )
+
+    private fun requestRequiredPermissions() {
+        EasyPermissions.requestPermissions(
+            this,
+            getString(R.string.permissions_rationale),
+            PERMISSION_LOCATION_WIFI_STATE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.CHANGE_WIFI_STATE
+        )
     }
 
 }
