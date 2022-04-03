a7ec3b520eab5183348fd1545da27516d257b553
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2022-04-03 20:42:43 +0200

    Add UI Test for MainActivity

---
+++ app/src/debug/kotlin/com/scribblex/rssi/ui/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.scribblex.rssi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity(){
+}
